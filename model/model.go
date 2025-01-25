3d64499509975b660c6e1211d7912bb93a7f49cf
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-01-25 23:58:44 +0530

    updated model

--- model/model.go
+++ model/model.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Movies struct {
 	ID          primitive.ObjectID `json:"_id",omitempty" bson:"_id,omitempty"`
 	Name        string             `json:"name,omitempty"`
-	Description string             `json:"name,omitempty"`
+	Description string             `json:"description,omitempty"`
 	Image       Images             `json:"image,omitempty"`
 }
 
