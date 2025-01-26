f0b35d9e6ba1612d4faa2d1c261b9059fa6c7377
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-01-26 23:57:44 +0530

    added user , wishlist and history model

--- model/model.go
+++ model/model.go
@@ -2,8 +2,18 @@ package model
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type User struct {
+	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Username     string             `json:"username,omitempty"`
+	Email        string             `json:"email,omitempty"`
+	PhoneNo      int                `json:"_"`
+	ProfileImage string             `json:"image,omitempty"`
+	Wishlist     Wishlist
+	History      History
+}
+
 type Movies struct {
-	ID          primitive.ObjectID `json:"_id",omitempty" bson:"_id,omitempty"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name        string             `json:"name,omitempty"`
 	Description string             `json:"description,omitempty"`
 	Image       Images             `json:"image,omitempty"`
@@ -12,4 +22,14 @@ type Movies struct {
 type Images struct {
 	Main   string `json:"main,omitempty"`
 	Poster string `json:"poster,omitempty"`
+}
+
+type Wishlist struct {
+	UserId primitive.ObjectID `json:"_id,omitempty"`
+	Movies []Movies           `json:"movies,omitempty"`
+}
+
+type History struct {
+	UserId primitive.ObjectID `json:"_id,omitempty"`
+	Movies []Movies           `json:"movies,omitempty"`
 }
