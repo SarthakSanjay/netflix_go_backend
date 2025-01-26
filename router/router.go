6de68fd7426a6bb941fe72bd028384a58e64c3b5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-01-26 23:58:09 +0530

    updated

--- router/router.go
+++ router/router.go
@@ -2,14 +2,14 @@ package router
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/sarthaksanjay/netflix-go/controller"
+	movieController "github.com/sarthaksanjay/netflix-go/controller"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
-	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movies", movieController.GetAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", movieController.CreateMovie).Methods("POST")
 
 	return router
 }
