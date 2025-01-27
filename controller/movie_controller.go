338937d6d6d679485a903afbbeb978c6f2f00ec8
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-01-27 23:50:32 +0530

    refactored movie controllers

---
+++ controller/movie_controller.go
@@ -0,0 +1,87 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	helper "github.com/sarthaksanjay/netflix-go/helper"
+	"github.com/sarthaksanjay/netflix-go/model"
+	"github.com/sarthaksanjay/netflix-go/utils"
+)
+
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := helper.GetAllMovie(r.Context())
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Failed to fetch movies"}, http.StatusInternalServerError)
+		return
+	}
+
+	utils.SendJSONResponse(w, movies, http.StatusOK)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	var movie model.Movies
+
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Invalid request body"}, http.StatusBadRequest)
+		return
+	}
+
+	id, err := helper.InsertMovie(movie)
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Failed to insert movie"}, http.StatusInternalServerError)
+		return
+	}
+	respose := map[string]interface{}{
+		"message":         "success",
+		"insertedMovieId": id.Hex(),
+		"movie":           &movie,
+	}
+	utils.SendJSONResponse(w, respose, http.StatusCreated)
+}
+
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	if len(params["id"]) != 24 {
+		utils.SendJSONResponse(w, map[string]string{"error": "Invalid movie id"}, http.StatusBadRequest)
+		return
+	}
+	count, err := helper.DeleteMovie(params["id"])
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Failed to delete movie"}, http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message": "successfully deleted movie",
+		"id":      params["id"],
+		"count":   count,
+	}
+
+	utils.SendJSONResponse(w, response, http.StatusOK)
+}
+
+func DeleteAllMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Allow-Content-Allow-Methods", "DELETE")
+
+	count := helper.DeleteAllMovie()
+
+	json.NewEncoder(w).Encode(count)
+}
+
+func GetMovieById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Allow-Content-Allow-Methods", "GET")
+
+	params := mux.Vars(r)
+	movie, err := helper.GetMovieById(params["id"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(movie)
+}
