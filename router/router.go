daa64b43e49fb4da90d452d0b4c257e258931f4d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-01-27 23:52:47 +0530

    updated

--- router/router.go
+++ router/router.go
@@ -2,14 +2,31 @@ package router
 
 import (
 	"github.com/gorilla/mux"
-	movieController "github.com/sarthaksanjay/netflix-go/controller"
+	"github.com/sarthaksanjay/netflix-go/controller"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/movies", movieController.GetAllMovies).Methods("GET")
-	router.HandleFunc("/api/movie", movieController.CreateMovie).Methods("POST")
+	// user routes
+
+	router.HandleFunc("/api/user", controller.CreateUser).Methods("POST")
+	router.HandleFunc("/api/user/{id}", controller.GetUser).Methods("GET")
+	router.HandleFunc("/api/user/{id}", controller.UpdateUser).Methods("PUT")
+	router.HandleFunc("/api/user/{id}", controller.DeleteUser).Methods("DELETE")
+	router.HandleFunc("/api/users", controller.DeleteAllUser).Methods("DELETE")
+	router.HandleFunc("/api/users", controller.CreateUser).Methods("GET")
+
+	// movies routes
+	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
+	router.HandleFunc("/api/movies", controller.DeleteAllMovie).Methods("DELETE")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.GetMovieById).Methods("GET")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteMovie).Methods("DELETE")
+
+	// watchlist routes
+
+	// router.HandleFunc("/api/watchlist/{id}", .GetUserWatchlist())
 
 	return router
 }
