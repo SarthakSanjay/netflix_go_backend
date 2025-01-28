28b4085f1f6ae36915561411fcdbd07287e1794c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-01-28 21:31:51 +0530

    added search, similar and popular movie controllers

--- controller/movie_controller.go
+++ controller/movie_controller.go
@@ -18,7 +18,13 @@ func GetAllMovies(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utils.SendJSONResponse(w, movies, http.StatusOK)
+	response := map[string]interface{}{
+		"message": "success",
+		"total":   len(movies),
+		"movies":  movies,
+	}
+
+	utils.SendJSONResponse(w, response, http.StatusOK)
 }
 
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
@@ -120,14 +126,68 @@ func UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	utils.SendJSONResponse(w, response, http.StatusOK)
 }
 
-// func SearchMovie() {
-// }
+func SearchMovie(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("q")
+	if query == "" {
+		utils.SendJSONResponse(w, map[string]string{"error": "Missing 'q' query params"}, http.StatusBadRequest)
+		return
+	}
+
+	movies, err := helper.SearchMovie(query)
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Movies not found"}, http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message": "movie found",
+		"total":   len(movies),
+		"movies":  movies,
+	}
+
+	utils.SendJSONResponse(w, response, http.StatusOK)
+}
+
+func PopularMovie(w http.ResponseWriter, r *http.Request) {
+	movies, err := helper.PopularMovie()
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Failed to fetch movies"}, http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message": "found popular movies",
+		"total":   len(movies),
+		"movies":  movies,
+	}
+
+	utils.SendJSONResponse(w, response, http.StatusOK)
+}
+
 //
-// func PopularMovie() {
+// func RecommendedMovie(w http.ResponseWriter , r *http.Request) {
 // }
-//
-// func RecommendedMovie() {
-// }
-//
-// func SimilarMovie() {
-// }
+
+func SimilarMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	movie, err := helper.GetMovieById(params["id"])
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Movie not found"}, http.StatusInternalServerError)
+		return
+	}
+
+	similarMovie, err := helper.SimilarMovie(movie.Genre)
+	if err != nil {
+		utils.SendJSONResponse(w, map[string]string{"error": "Failed to get similar movie"}, http.StatusInternalServerError)
+		return
+	}
+
+	res := map[string]interface{}{
+		"message":      "success",
+		"total":        len(similarMovie),
+		"similarMovie": similarMovie,
+	}
+
+	utils.SendJSONResponse(w, res, http.StatusOK)
+}
