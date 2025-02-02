4ad4d6154b74d17bebff95c3fead1e43e75bb920
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-02 23:54:03 +0530

    added token services

---
+++ services/token_service.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var (
+	accessSecret  = os.Getenv("ACCESS_SECRET")
+	refreshSecret = os.Getenv("REFRESH_SECRET")
+	resetSecret   = os.Getenv("RESET_SECRET")
+)
+
+type Claims struct {
+	jwt.RegisteredClaims
+	Email  string             `json:"email,omitempty"`
+	UserId primitive.ObjectID `json:"userId,omitempty"`
+}
+
+func GenerateJWT(userId primitive.ObjectID, email string, secret []byte, expiry time.Duration) (string, error) {
+	claims := &Claims{
+		UserId: userId,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiry)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func GenerateAccessToken(userId primitive.ObjectID, email string) (string, error) {
+	return GenerateJWT(userId, email, []byte(accessSecret), 15*time.Minute)
+}
+
+func GenerateRefreshToken(userId primitive.ObjectID, email string) (string, error) {
+	return GenerateJWT(userId, email, []byte(refreshSecret), 7*24*time.Hour)
+}
+
+func GenerateResetToken(userId primitive.ObjectID, email string) (string, error) {
+	return GenerateJWT(userId, email, []byte(refreshSecret), 15*time.Minute)
+}
+
+func VerifyToken(tokenString string, secret []byte) (*jwt.Token, *Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return secret, nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	return token, claims, nil
+}
