b14fa62dfd4ab89b0d8f84567a48b2fd570b00e5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-02 23:56:38 +0530

    added protected routes

--- router/router.go
+++ router/router.go
@@ -3,31 +3,37 @@ package router
 import (
 	"github.com/gorilla/mux"
 	"github.com/sarthaksanjay/netflix-go/controller"
+	"github.com/sarthaksanjay/netflix-go/middleware"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/api/user", controller.CreateUser).Methods("POST")
+	router.HandleFunc("/api/user/login", controller.LoginUser).Methods("POST")
 	// user routes
 
-	// router.HandleFunc("/api/user", controller.CreateUser).Methods("POST")
-	// router.HandleFunc("/api/user/{id}", controller.GetUser).Methods("GET")
-	// router.HandleFunc("/api/user/{id}", controller.UpdateUser).Methods("PUT")
-	// router.HandleFunc("/api/user/{id}", controller.DeleteUser).Methods("DELETE")
-	// router.HandleFunc("/api/users", controller.DeleteAllUser).Methods("DELETE")
-	// router.HandleFunc("/api/users", controller.CreateUser).Methods("GET")
+	protectedRoutes := router.PathPrefix("/api").Subrouter()
+	protectedRoutes.Use(middleware.AuthMiddleware)
+
+	// protectedRoutes.HandleFunc("/api/user/{id}", controller.GetUser).Methods("GET")
+	// protectedRoutes.HandleFunc("/api/user/{id}", controller.UpdateUser).Methods("PUT")
+	// protectedRoutes.HandleFunc("/api/user/{id}", controller.DeleteUser).Methods("DELETE")
+	// protectedRoutes.HandleFunc("/api/users", controller.DeleteAllUser).Methods("DELETE")
+	protectedRoutes.HandleFunc("/users", controller.GetAllUser).Methods("GET")
 
 	// movies routes
-	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
-	router.HandleFunc("/api/movies", controller.DeleteAllMovie).Methods("DELETE")
-	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
-	router.HandleFunc("/api/movie/{id}", controller.GetMovieById).Methods("GET")
-	router.HandleFunc("/api/movie/{id}", controller.DeleteMovie).Methods("DELETE")
-	router.HandleFunc("/api/movie/{id}", controller.UpdateMovie).Methods("PUT")
-	router.HandleFunc("/api/movie/search", controller.SearchMovie).Methods("GET")
-	router.HandleFunc("/api/movie/popular", controller.PopularMovie).Methods("GET")
-	// router.HandleFunc("/api/movie/recommended", controller.RecommendedMovie).Methods("GET")
-	router.HandleFunc("/api/movie/{id}/similar", controller.SimilarMovie).Methods("GET")
+	protectedRoutes.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
+	protectedRoutes.HandleFunc("/api/movies", controller.DeleteAllMovie).Methods("DELETE")
+	protectedRoutes.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	protectedRoutes.HandleFunc("/api/movie/{id}", controller.GetMovieById).Methods("GET")
+	protectedRoutes.HandleFunc("/api/movie/{id}", controller.DeleteMovie).Methods("DELETE")
+	protectedRoutes.HandleFunc("/api/movie/{id}", controller.UpdateMovie).Methods("PUT")
+	protectedRoutes.HandleFunc("/api/movie/search", controller.SearchMovie).Methods("GET")
+	protectedRoutes.HandleFunc("/api/movie/popular", controller.PopularMovie).Methods("GET")
+	// protectedRoutes.HandleFunc("/api/movie/recommended", controller.RecommendedMovie).Methods("GET")
+	protectedRoutes.HandleFunc("/api/movie/{id}/similar", controller.SimilarMovie).Methods("GET")
+
 	//
 	// watchlist routes
 
