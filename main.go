b7f043f0b455dc7b943fb4d5c7982b4f0dbe39a6
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-02 23:57:15 +0530

    refactored

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func main() {
 	r := router.Router()
 	fmt.Println("Server is getting started...")
 
-	log.Fatal(http.ListenAndServe(":4000", r))
 	fmt.Println("Server listing on port 4000")
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
