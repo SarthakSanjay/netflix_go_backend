e4bed0e92952a8a99094735f609f079b4898c6d0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-05 23:18:48 +0530

    added cors

--- main.go
+++ main.go
@@ -5,14 +5,35 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"github.com/sarthaksanjay/netflix-go/db"
 	"github.com/sarthaksanjay/netflix-go/router"
 )
 
 func main() {
 	fmt.Println("Netflix Apis")
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
 	r := router.Router()
+	db.ConnectDB()
+	defer db.DisconnectDB()
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // todo:in production use actual clien url
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true, // Allow cookies and credentials
+	})
+
+	// wrapped router with cors middleware
+	handler := c.Handler(r)
+
 	fmt.Println("Server is getting started...")
 
 	fmt.Println("Server listing on port 4000")
-	log.Fatal(http.ListenAndServe(":4000", r))
+	log.Fatal(http.ListenAndServe(":4000", handler))
 }
