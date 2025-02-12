c369296606654e43c746c7fcd97cb992b074b055
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-12 14:46:30 +0530

    added getAllUserProfile,updateProfile and deleteProfile helpers

--- helper/profile_helper.go
+++ helper/profile_helper.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/sarthaksanjay/netflix-go/db"
 	"github.com/sarthaksanjay/netflix-go/model"
@@ -35,4 +36,69 @@ func GetProfileById(profileId string) (model.Profile, error) {
 	}
 
 	return profile, nil
+}
+
+func GetAllUserProfiles(userId string) ([]model.Profile, error) {
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		log.Println("Invalid UserId")
+		return nil, err
+	}
+
+	filter := bson.M{"userId": id}
+	cursor, err := db.ProfileCollection.Find(context.Background(), filter)
+	if err != nil {
+		log.Println("Unable to find profiles with given userId")
+		return nil, err
+	}
+
+	var profiles []model.Profile
+	if err := cursor.All(context.Background(), &profiles); err != nil {
+		return nil, err
+	}
+
+	return profiles, nil
+}
+
+func UpdateProfile(profileId string, updates model.Profile) (int, error) {
+	id, err := primitive.ObjectIDFromHex(profileId)
+	if err != nil {
+		log.Println("Invalid ProfileId")
+		return 0, err
+	}
+	newProfile := bson.M{}
+	if updates.Name != "" {
+		newProfile["name"] = updates.Name
+	}
+	if updates.Avatar != "" {
+		newProfile["avatar"] = updates.Avatar
+	}
+
+	newProfile["updatedAt"] = time.Now()
+
+	update := bson.M{"$set": newProfile}
+	result, err := db.ProfileCollection.UpdateByID(context.Background(), id, update)
+	if err != nil {
+		log.Println("Error updating user profile", err)
+		return 0, err
+	}
+
+	return int(result.ModifiedCount), nil
+}
+
+func DeleteProfile(profileId string) (int, error) {
+	id, err := primitive.ObjectIDFromHex(profileId)
+	if err != nil {
+		log.Println("Invalid ProfileId")
+		return 0, err
+	}
+
+	filter := bson.M{"_id": id}
+	result, err := db.ProfileCollection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Println("Unable to delete user profile")
+		return 0, err
+	}
+
+	return int(result.DeletedCount), nil
 }
