5b2d639138f698add1cd97ccdbb30d4251b4705c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-16 22:40:54 +0530

    added logout controller

---
+++ controller/logout_controller.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/sarthaksanjay/netflix-go/dto"
+	"github.com/sarthaksanjay/netflix-go/helper"
+	"github.com/sarthaksanjay/netflix-go/services"
+	"github.com/sarthaksanjay/netflix-go/utils"
+)
+
+func LogoutUser(w http.ResponseWriter, r *http.Request) {
+	log.Println("hello")
+	id, err := ExtractUserIdFromContext(r.Context())
+	if err != nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	fmt.Println("actual id after extraction ", id)
+
+	userId := id.Hex()
+	fmt.Printf("Type of user id %T", userId)
+
+	updateCount, err := helper.LogoutUser(userId)
+	if err != nil {
+		utils.SendJSONResponse(w, dto.ErrorResponseDTO{Error: "Error logging out user"}, http.StatusInternalServerError)
+		return
+	}
+
+	services.ClearTokenCookies(w, "access_token")
+	services.ClearTokenCookies(w, "refresh_token")
+
+	utils.SendJSONResponse(w, dto.SuccessResponse{
+		Message: "success",
+		Data:    updateCount,
+	}, http.StatusOK)
+}
