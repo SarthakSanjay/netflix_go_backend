2cc2b7a70c7ad73dd6a6d13e3f014bedabafb960
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-05 19:07:33 +0530

    add: movies success response dto

--- dto/success_res_dto.go
+++ dto/success_res_dto.go
@@ -1,6 +1,14 @@
 package dto
 
+import "github.com/sarthaksanjay/netflix-go/model"
+
 type SuccessResponse struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+type MovieSuccessResponse struct {
+	Message string         `json:"message"`
+	Movies  []model.Movies `json:"message"`
+	Total   int            `json:"total"`
 }
