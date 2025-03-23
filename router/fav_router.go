01352d33df8c89d28681db278aa952f7aa7abb00
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-23 23:17:20 +0530

    add: fav router

--- router/fav_router.go
+++ router/fav_router.go
@@ -6,7 +6,9 @@ import (
 )
 
 func FavRoutes(protectedRoutes *mux.Router) {
-	protectedRoutes.HandleFunc("/favorite", controller.AddContentToFavorite).Methods("POST")
-	protectedRoutes.HandleFunc("/favorite/{profileId}/{contentId}", controller.RemoveContentFromFavorite).Methods("DELETE")
-	protectedRoutes.HandleFunc("/favorites/{id}", controller.GetAllContentFromUsersProfileFavorite).Methods("GET")
+	protectedRoutes.HandleFunc("/favorite", controller.AddMovieToFavorite).Methods("POST")
+	protectedRoutes.HandleFunc("/favorite/{profileId}/{contentId}", controller.RemoveMovieFromFavorite).Methods("DELETE")
+	protectedRoutes.HandleFunc("/favorites/{id}", controller.GetAllMoviesFromUsersProfileFavorite).Methods("GET")
+	protectedRoutes.HandleFunc("/favorite", controller.AddShowToFavorite).Methods("POST")
+	protectedRoutes.HandleFunc("/favorite/{profileId}/{contentId}", controller.RemoveShowFromFavorite).Methods("DELETE")
 }
