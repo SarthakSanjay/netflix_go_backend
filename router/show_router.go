26925337247353b4ce75728aa8d512c31bec6332
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-04 22:27:10 +0530

    update: add routes

--- router/show_router.go
+++ router/show_router.go
@@ -9,15 +9,17 @@ func ShowRoutes(protectedRoutes *mux.Router) {
 	// shows routes
 	protectedRoutes.HandleFunc("/shows", controller.GetAllShows).Methods("GET")
 	// protectedRoutes.HandleFunc("/shows", controller.DeleteAllShows).Methods("DELETE")
-	// protectedRoutes.HandleFunc("/show", controller.CreateShow).Methods("POST")
+	protectedRoutes.HandleFunc("/show", controller.CreateShow).Methods("POST")
 	protectedRoutes.HandleFunc("/show/{id}", controller.GetShowById).Methods("GET")
-	// protectedRoutes.HandleFunc("/show/{id}", controller.DeleteShow).Methods("DELETE")
+	protectedRoutes.HandleFunc("/{contentType}/{id}", controller.DeleteShow).Methods("DELETE")
+	protectedRoutes.HandleFunc("/shows", controller.DeleteAllShow).Methods("DELETE")
+
 	// protectedRoutes.HandleFunc("/show/{id}", controller.UpdateShow).Methods("PUT")
 	// protectedRoutes.HandleFunc("/show/search", controller.SearchShow).Methods("GET")
 	// protectedRoutes.HandleFunc("/show/popular", controller.PopularShows).Methods("GET")
 	// protectedRoutes.HandleFunc("/movie/recommended", controller.RecommendedMovie).Methods("GET")
 	// protectedRoutes.HandleFunc("/show/{id}/similar", controller.SimilarShows).Methods("GET")
-	protectedRoutes.HandleFunc("/show/{genre}", controller.GetShowByGenre).Methods(("GET"))
+	protectedRoutes.HandleFunc("/shows/{genre}", controller.GetShowByGenre).Methods(("GET"))
 
-	protectedRoutes.HandleFunc("/show/trending", controller.GetTrendingShows).Methods("GET")
+	protectedRoutes.HandleFunc("/shows/trending", controller.GetTrendingShows).Methods("GET")
 }
