ef63eef83effd51ffb7a4310630c2d331be3c9c7
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-09 23:46:24 +0530

    update: add more fields to cast model

--- model/cast.go
+++ model/cast.go
@@ -1,6 +1,12 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Cast struct {
-	Name  string `json:"name,omitempty" bson:"name,omitempty"`
-	Image string `json:"image,omitempty" bson:"image,omitempty"`
+	Name   string             `json:"name,omitempty" bson:"name,omitempty"`
+	Image  string             `json:"image,omitempty" bson:"image,omitempty"`
+	Gender string             `json:"gender,omitempty" bson:"gender,omitempty"`
+	Age    string             `json:"age,omitempty" bson:"age,omitempty"`
+	Bio    string             `json:"bio,omitempty" bson:"bio,omitempty"`
+	Id     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 }
