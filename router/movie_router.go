8b4cfd0ee20cffccb89f12cce233057d83aa63ea
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-11 23:26:34 +0530

    refactor

--- router/movie_router.go
+++ router/movie_router.go
@@ -17,7 +17,7 @@ func MovieRoutes(protectedRoutes *mux.Router) {
 	protectedRoutes.HandleFunc("/movie/popular", controller.PopularMovie).Methods("GET")
 	// protectedRoutes.HandleFunc("/movie/recommended", controller.RecommendedMovie).Methods("GET")
 	protectedRoutes.HandleFunc("/movie/{id}/similar", controller.SimilarMovie).Methods("GET")
-	protectedRoutes.HandleFunc("/movies/{genre}", controller.GetMoviesByGenre).Methods(("GET"))
+	protectedRoutes.HandleFunc("/movies/{genre}", controller.GetMoviesByGenre).Methods("GET")
 
-	protectedRoutes.HandleFunc("/movies/trending", controller.GetTrendingMovies).Methods("GET")
+	protectedRoutes.HandleFunc("/trending/movies", controller.GetTrendingMovies).Methods("GET")
 }
