e93f0fcf3c637395f4f111874cc10e7880455b6a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-20 22:06:46 +0530

    update: watchlist routes

--- router/watchlist_router.go
+++ router/watchlist_router.go
@@ -6,9 +6,10 @@ import (
 )
 
 func WatchlistRoutes(protectedRoutes *mux.Router) {
-	protectedRoutes.HandleFunc("/watchlist", controller.AddMovieToWatchlist).Methods("POST")
+	protectedRoutes.HandleFunc("/watchlist/movie", controller.AddMovieToWatchlist).Methods("POST")
 	protectedRoutes.HandleFunc("/watchlist/movie/{id}", controller.GetMoviesFromUserWatchlist).Methods("GET")
 	protectedRoutes.HandleFunc("/watchlist/{profileId}/{contentId}", controller.DeleteContentFromWatchlist).Methods("DELETE")
 	protectedRoutes.HandleFunc("/watchlist/all/{contentType}/{id}", controller.DeleteAllContentFromWatchlist).Methods("DELETE")
 	protectedRoutes.HandleFunc("/watchlist/show/{id}", controller.GetShowsFromUserWatchlist).Methods("GET")
+	protectedRoutes.HandleFunc("/watchlist/show", controller.AddShowToWatchlist).Methods("POST")
 }
